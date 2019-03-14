aa644892b9b0f394945e82529df63e3f595a3f96
thefourhorsemen-4 <fourhorsemen987@gmail.com>
2019-03-14 09:01:21 +0700

    asdsad

--- src/app/homepage/header/header.component.ts
+++ src/app/homepage/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { TokenStorageService } from '../../shared/token-storage.service';
+import {Component, OnInit} from '@angular/core';
+import {TokenStorageService} from '../../shared/token-storage.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,9 @@ import { TokenStorageService } from '../../shared/token-storage.service';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private token: TokenStorageService) { }
+  constructor(private token: TokenStorageService,
+              private router: Router) {
+  }
 
   ngOnInit() {
   }
--- src/app/shared/token-storage.service.ts
+++ src/app/shared/token-storage.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 const TOKEN_KEY = 'AuthToken';
 const USERNAME_KEY = 'AuthUsername';
@@ -9,22 +9,24 @@ const AUTHORITIES_KEY = 'AuthAuthorities';
 })
 export class TokenStorageService {
   private roles: Array<string> = [];
-  constructor() { }
 
-  signOut(){
+  constructor() {
+  }
+
+  signOut() {
     window.sessionStorage.clear();
   }
 
-  public saveToken(token: string){
+  public saveToken(token: string) {
     window.sessionStorage.removeItem(TOKEN_KEY);
     window.sessionStorage.setItem(TOKEN_KEY, token);
   }
-  
-  public getToken(): string{
+
+  public getToken(): string {
     return sessionStorage.getItem(TOKEN_KEY);
   }
 
-  public saveUsername(username: string){
+  public saveUsername(username: string) {
     window.sessionStorage.removeItem(USERNAME_KEY);
     window.sessionStorage.setItem(USERNAME_KEY, username);
   }
@@ -32,21 +34,21 @@ export class TokenStorageService {
   public getUsername(): string {
     return sessionStorage.getItem(USERNAME_KEY);
   }
- 
+
   public saveAuthorities(authorities: string[]) {
     window.sessionStorage.removeItem(AUTHORITIES_KEY);
     window.sessionStorage.setItem(AUTHORITIES_KEY, JSON.stringify(authorities));
   }
- 
+
   public getAuthorities(): string[] {
     this.roles = [];
- 
+
     if (sessionStorage.getItem(TOKEN_KEY)) {
       JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)).forEach(authority => {
         this.roles.push(authority.authority);
       });
     }
- 
+
     return this.roles;
   }
 }
